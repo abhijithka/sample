10f2566ba3e0656298873e4bd9c5259f3e3df839
abhijithka <kaabhijith27.7@gmail.com>
2013-12-05 13:08:29 +0530

    edited connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 require_once('config.php');
-$sqlobj = mysqli_connect("localhost","root","9745025172","new");
+$sqlobj = mysqli_connect($host,$username,$password,$db_name);
 if(mysqli_connect_errno())
 {
 	printf("connection failed",mysqli_connect_error());
